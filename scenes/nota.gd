e38a727998e8fc0271aa26e612c17015266304d1
Felipe <felipeb@clubefii.com.br>
2026-06-30 11:27:58 -0300

    Atualiza caminhos dos sprites de notas e adiciona auto-destroy fora da tela

--- scenes/nota.gd
+++ scenes/nota.gd
@@ -3,14 +3,16 @@ extends Area2D
 @export var speed = 500.0
 var tipo = 0
 
-const SPRITE_SOLTO = preload("res://assets/sprites/nota_solto.png")
-const SPRITE_CHIADO = preload("res://assets/sprites/nota_chiado.png")
-const SPRITE_PRESO = preload("res://assets/sprites/nota_preso.png")
+const SPRITE_SOLTO = preload("res://assets/sprites/notas/solto.png")
+const SPRITE_CHIADO = preload("res://assets/sprites/notas/chiado.png")
+const SPRITE_PRESO = preload("res://assets/sprites/notas/preso.png")
 
 @onready var sprite = $Sprite2D
 
 func _process(delta):
 	position.y += speed * delta
+	if position.y > get_viewport_rect().size.y + 100:
+		queue_free()
 
 func setup(tipo_da_nota):
 	match tipo_da_nota:
